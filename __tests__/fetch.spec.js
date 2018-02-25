e00005a260a9652089ba60772dd65f7673dda3dc
arnaud ferrand <arferrand@gmail.com>
2018-02-25 12:29:17 +0000

    fetch UT

--- __tests__/fetch.spec.js
+++ __tests__/fetch.spec.js
@@ -4,5 +4,76 @@ describe('fetch', () => {
   test('Library exists', () => {
     expect(fetch).toBeTruthy();
   });
-  describe('checkStatus function', () => {});
+
+  describe('fetchUrl function', () => {
+    test('returns an object', () => {
+      const url = 'https://google.com';
+      const p = fetch.fetchUrl(url);
+      expect(typeof p).toEqual('object');
+    });
+  });
+
+  describe('fetch function', () => {
+    test('returns an object', () => {
+      expect.assertions(1);
+
+      const url = 'http://httpbin.org/ip';
+      return new Promise((resolve, reject) => {
+        fetch(url, resolve, reject);
+      })
+        .then(data => {
+          expect(data).toBeTruthy();
+        })
+        .catch(() => {
+          // no used, in case it raises a test error
+          expect(false).toBeTruthy();
+        });
+    });
+    test('Reject non json', () => {
+      expect.assertions(1);
+
+      const url = 'http://google.com';
+      return new Promise((resolve, reject) => {
+        fetch(url, resolve, reject);
+      })
+        .then(() => {
+          // no used, in case it raises a test error
+          expect(false).toBeTruthy();
+        })
+        .catch(data => {
+          expect(data).toBeTruthy();
+        });
+    });
+  });
+
+  describe('parseJSON function', () => {
+    test('json', () => {
+      const data = {
+        foor: 'bar',
+      };
+      const response = {
+        json: () => data,
+      };
+      expect(fetch.parseJSON(response)).toEqual(data);
+    });
+  });
+
+  describe('checkStatus function', () => {
+    test('status 200', () => {
+      const response = {
+        status: 200,
+        statusText: 'OK',
+      };
+      expect(fetch.checkStatus(response)).toBe(response);
+    });
+    test('status 403', () => {
+      const response = {
+        status: 403,
+        statusText: 'Forbidden',
+      };
+      expect(() => {
+        fetch.checkStatus(response);
+      }).toThrow();
+    });
+  });
 });
