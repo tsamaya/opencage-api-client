5a99ea87715c1cfa33e071efff22e53b5c753d4e
arnaud ferrand <arferrand@gmail.com>
2018-02-26 17:18:00 +0000

    dotenv at root source, so browser bundler will not need to know about env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const opencage = require('./src');
 
 module.exports = opencage;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const geocode = require('./geocode');
 
 module.exports = {
