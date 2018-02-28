0e700aa144a002bb67e3b86c23b53c38118b07e4
arnaud ferrand <arferrand@gmail.com>
2018-02-28 20:54:41 +0000

    rename node sample

--- samples/node-sample.js
+++ samples/node-sample.js
@@ -2,14 +2,14 @@
 const opencage = require('../');
 
 opencage.geocode({q: 'rue de la république lyon', limit: 3}).then(data => {
-  console.log('geocode -----------------------------');
+  console.log('geocode ---------------------------------');
   console.log(JSON.stringify(data));
 }).catch(error => {
   console.log('error', error.message);
 });
 
 opencage.geocode({q: '45.188529 5.724524'}).then(data => {
-  console.log('reverse geocode -----------------------------');
+  console.log('reverse geocode -------------------------');
   console.log(JSON.stringify(data));
 }).catch(error => {
   console.log('error', error.message);
