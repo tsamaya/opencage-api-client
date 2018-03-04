13bcf4d6da5e90bbde184cf55dc552df59804ce2
arnaud ferrand <arferrand@gmail.com>
2018-03-04 15:26:35 +0000

    reject missing input parameters

--- src/geocode.js
+++ src/geocode.js
@@ -6,8 +6,8 @@ const OPEN_CAGE_DATA_URL = 'https://api.opencagedata.com/geocode/v1/json';
 
 /**
  * @private
- * build input paraameters with lat,lng,query and optional paraameters
- * TODO
+ * build input paraameters with lat,lng,query and optional parameters
+ * TODO externalize for gecodeGeoJSON method
  *
  * @param  {Object} input [description]
  * @return {Object}       the OpenCage API parameters
@@ -16,11 +16,22 @@ const buildParams = input => input;
 
 /**
  * geocode
- * @param  {Object} input query paraameters
- * @return {Promise}      a promise resolved by the json format API payload
+ * @param  {Object} input   query paraameters
+ * @return {Promise}        a promise resolved by the json format API payload
  */
 const geocode = input =>
   new Promise((resolve, reject) => {
+    if (helpers.isUndefinedOrNull(input)) {
+      const error = new Error('missing input parameters');
+      error.response = {
+        status: {
+          code: 400,
+          message: 'missing input parameters',
+        },
+      };
+      reject(error);
+      return;
+    }
     const params = buildParams(input);
     if (helpers.isUndefinedOrEmpty(params.key)) {
       params.key = process.env.OCD_API_KEY;
