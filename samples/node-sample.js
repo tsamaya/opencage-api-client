7bde8e2eae140d7f2dabc0daac4c96ad6db90e6a
arnaud ferrand <arferrand@gmail.com>
2018-07-16 10:43:54 +0100

    prettier format sample files

--- samples/node-sample.js
+++ samples/node-sample.js
@@ -1,30 +1,22 @@
 /* eslint-disable */
 const opencage = require('../');
 
-opencage.geocode({q: 'rue de la république lyon', limit: 3}).then(data => {
-  console.log('geocode ---------------------------------');
-  console.log(JSON.stringify(data));
-}).catch(error => {
-  console.log('error', error.message);
-});
+opencage
+  .geocode({ q: 'rue de la république lyon', limit: 3 })
+  .then(data => {
+    console.log('geocode ---------------------------------');
+    console.log(JSON.stringify(data));
+  })
+  .catch(error => {
+    console.log('error', error.message);
+  });
 
-opencage.geocode({q: '45.188529 5.724524'}).then(data => {
-  console.log('reverse geocode -------------------------');
-  console.log(JSON.stringify(data));
-}).catch(error => {
-  console.log('error', error.message);
-});
-//
-// opencage.geocode('rue de la république lyon').then(data => {
-//   console.log('geocode with string ---------------------');
-//   console.log(JSON.stringify(data));
-// }).catch(error => {
-//   console.log('error', error.message);
-// });
-//
-// opencage.geocode('rue de la république lyon', {limit: 3}).then(data => {
-//   console.log('geocode with string and options ---------');
-//   console.log(JSON.stringify(data));
-// }).catch(error => {
-//   console.log('error', error.message);
-// });
+opencage
+  .geocode({ q: '45.188529 5.724524' })
+  .then(data => {
+    console.log('reverse geocode -------------------------');
+    console.log(JSON.stringify(data));
+  })
+  .catch(error => {
+    console.log('error', error.message);
+  });
