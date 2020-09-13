cc841588a162841034245234ac627f2289f136db
Arnaud Ferrand <arferrand@gmail.com>
2020-09-13 19:45:12 +0100

    interface GeocodeRequest

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,27 @@
 declare module 'opencage-api-client' {
-  export function geocode(input: any): Promise<any>;
+  enum Option {
+    On = 1,
+    Off = 0,
+  }
+
+  interface GeocodeRequest {
+    key?: string;
+    q: string;
+    abbrv?: Option;
+    add_request?: Option;
+    bounds?: string;
+    countrycode?: string;
+    jsonp?: string;
+    language?: string;
+    limit?: number;
+    min_confidence?: number;
+    no_annotations?: Option;
+    no_dedupe?: Option;
+    no_record?: Option;
+    pretty?: Option;
+    proximity?: string;
+    roadinfo?: Option;
+  }
+
+  export function geocode(input: GeocodeRequest): Promise<any>;
 }
