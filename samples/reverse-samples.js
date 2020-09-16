5fb40efbc46e38dedc5001879735a3166ebb9e6e
Arnaud Ferrand <arferrand@gmail.com>
2020-09-16 10:09:24 +0100

    adding reverse examples

---
+++ samples/reverse-samples.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+const opencage = require('../');
+
+opencage
+  .geocode({ q: '45.188529,5.724524', no_annotations: 1 })
+  .then((data) => {
+    console.log('reverse geocode  -------------------------');
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log('error', error.message);
+  });
+
+opencage
+  .geocode({ q: '+45.188529,+5.724524', no_annotations: 1 })
+  .then((data) => {
+    console.log('reverse geocode -------------------------');
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log('error', error.message);
+  });
+
+opencage
+  .geocode({ q: '45.188529 5.724524', no_annotations: 1 })
+  .then((data) => {
+    console.log('reverse geocode -------------------------');
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log('error', error.message);
+  });
